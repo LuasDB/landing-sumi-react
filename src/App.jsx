6e5590da3b73b63466715f295896c68e6acf98b9
Saul De la fuente <saul.delafuenteb@gmail.com>
2025-09-02 18:40:22 -0600

    Se modifica la base de react route dom

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <Routes>
       <Route path='/' element={<Landing />} />
-      <Route path='*/administrador' element={<Admin />} />
+      <Route path='/administrador' element={<Admin />} />
     </Routes>
   );
 }
